88e53e0783366520f11164ad30a5e2707ef822d5
tllach <tllach@uninorte.edu.co>
2023-11-11 23:15:46 -0500

    tweets filters were made

--- generador.py
+++ generador.py
@@ -14,21 +14,33 @@ def correct_filepath(path: str):
         path = path[1:]
     return path.replace('/', '\\').strip()
 
-def process_tweet(tweet, hashtags, authors, mentions, retweets, co_retweets):
-    # Procesa el tweet según tus necesidades
-    print(tweet)
-    # Registra las menciones y retweets
-    for mention in tweet.get('mentions', []):
-        mentions[mention] = mentions.get(mention, [])
-        mentions[mention].append(tweet['id'])
-    if 'retweet' in tweet:
-        retweeted_id = tweet['retweet']
-        retweets[retweeted_id] = retweets.get(retweeted_id, [])
-        retweets[retweeted_id].append(tweet['author'])
-        co_retweets[(tweet['author'], retweeted_id)] = co_retweets.get((tweet['author'], retweeted_id), 0) + 1
-        
-
-
+def process_tweets(input_directory: str, start_date, end_date, hashtags: list) -> list: 
+    tweets = []
+    for root, dirs, files in os.walk(input_directory):
+        for subdir in dirs:
+            new_path = os.path.join(root, subdir)
+            for subroot, subsdirs, subfiles in os.walk(new_path):
+                for file in subfiles:
+                    if file.endswith('.bz2'):
+                        print(file)
+                        with bz2.BZ2File(os.path.join(subroot, file), 'rb') as f:
+                            for line in f:
+                                line = line.decode('utf-8')
+                                tweet = json.loads(line)
+                                created_at = tweet.get('created_at')
+                                
+                                if (start_date or end_date and created_at) or len(hashtags) > 0:
+                                    tweet_date = datetime.strptime(created_at, '%a %b %d %H:%M:%S %z %Y').replace(tzinfo=None)
+                                    if start_date and not end_date and start_date <= tweet_date:
+                                        tweets.append(tweet)
+                                    if end_date and not start_date and tweet_date <= end_date:
+                                        tweets.append(tweet)
+                                    if end_date and start_date and (start_date <= tweet_date <= end_date):
+                                        tweets.append(tweet)
+                                    for hashtag in tweet['entities']['hashtags']:
+                                        if hashtag['text'] in hashtags:
+                                            tweets.pop()
+    return tweets
 
 
 
@@ -56,7 +68,6 @@ def main(argv):
     
     flag = False
     for opt, arg in opts:
-        #print("opt:" + opt + " arg: " + arg + " \n")
         if opt == '-d':
             arg = correct_filepath(arg)
             input_directory = arg
@@ -96,29 +107,10 @@ def main(argv):
     mentions = {}
     retweets = {}
     co_retweets = {}
-
-
-    for root, dirs, files in os.walk(input_directory):
-        for subdirs in dirs:
-            for root, sub_dirs, subfiles  in os.walk(os.path.join(root, subdirs)):
-                for file in subfiles:
-                    if file.endswith('.bz2'):
-                        with bz2.BZ2File(os.path.join(root, file), 'rb') as f:
-                            for tweet in f:
-                                tweet = tweet.decode('utf-8')
-                                tweet = json.loads(tweet)
-                                if(start_date or end_date) and not tweet.get('created_at') == None:
-                                    tweet_date = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y').replace(tzinfo=None)
-                                    if(start_date):
-                                        if (start_date <= tweet_date):
-                                            if not hashtags or any(hashtag.lower() in tweet['text'].lower() for hashtag in hashtags):
-                                                process_tweet(tweet, hashtags, authors, mentions, retweets, co_retweets)
-                                    if(end_date):
-                                        if (tweet_date <= end_date):
-                                            print('End_tweet_date: ' + str(tweet_date))
-                                            if not hashtags or any(hashtag.lower() in tweet['text'].lower() for hashtag in hashtags):
-                                                process_tweet(tweet, hashtags, authors, mentions, retweets, co_retweets)
-
+    
+    tweets = process_tweets(input_directory, start_date, end_date, hashtags)
+    
+    
 
 
 main("-d data/2016/01/06/ -fi 06-01-16 --grt")
