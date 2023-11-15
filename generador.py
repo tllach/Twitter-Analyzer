45895ffe61cb7542c135199bfcfb1e6fca8253d9
tllach <tllach@uninorte.edu.co>
2023-11-15 13:40:43 -0500

    generate_json_mention created

--- generador.py
+++ generador.py
@@ -1,8 +1,9 @@
 import os
 import networkx as nx 
 import json 
+import time
 import shutil,bz2,getopt,sys
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 import bz2
 from datetime import datetime
 import graphlib
@@ -13,44 +14,11 @@ def correct_filepath(path: str):
         path = path[1:]
     return path.replace('/', '\\').strip()
 
-def process_tweets(input_directory: str, start_date, end_date, hashtags: list) -> list: 
-    tweets = []
-    for root, dirs, files in os.walk(input_directory):
-        if dirs:
-            for subdir in dirs:
-                new_path = os.path.join(root, subdir)
-                process_tweets(new_path, start_date, end_date, hashtags)
-        for file in files:
-            if file.endswith('.bz2'):
-                with bz2.BZ2File(os.path.join(root, file), 'rb') as f:
-                    for line in f:
-                        try:
-                            line = line.decode('utf-8')
-                            tweet = json.loads(line)
-                            created_at = tweet.get('created_at')
-                            # Verifica si no hay restricciones de fecha ni hashtags
-                            if not start_date and not end_date and not hashtags:
-                                tweets.append(tweet)
-                            else:
-                                if not start_date and not end_date:
-                                    # Si no hay restricciones de fecha, verifica los hashtags
-                                    if hashtags and any(hashtag['text'] in hashtags for hashtag in tweet.get('entities', {}).get('hashtags', [])):
-                                        tweets.append(tweet)
-                                elif created_at:
-                                    # Si hay restricciones de fecha, verifica también la fecha y los hashtags
-                                        tweet_date = datetime.strptime(created_at, '%a %b %d %H:%M:%S %z %Y').replace(tzinfo=None)
-                                        if (start_date and tweet_date >= start_date) or (end_date and tweet_date <= end_date):
-                                            if not hashtags or any(hashtag['text'] in hashtags for hashtag in tweet.get('entities', {}).get('hashtags', [])):
-                                                tweets.append(tweet)
-                        except (json.JSONDecodeError, ValueError, TypeError) as e:
-                            print(f"Error processing tweet: {e}")
-    return tweets
-
 def is_valid_tweet(tweet, start_date, end_date, hashtags):
     created_at = tweet.get('created_at')
     if not start_date and not end_date and not hashtags:
         return True
-    if not start_date and not end_date:
+    if not start_date and not end_date and hashtags:
         return hashtags and any(hashtag['text'] in hashtags for hashtag in tweet.get('entities', {}).get('hashtags', []))
     if created_at:
         tweet_date = datetime.strptime(created_at, '%a %b %d %H:%M:%S %z %Y').replace(tzinfo=None)
@@ -79,9 +47,12 @@ def process_bz2_file(file_path, start_date, end_date, hashtags, tweets):
             except (json.JSONDecodeError, ValueError, TypeError) as e:
                 print(f"Error processing tweet: {e}")
 
-def process_tweets2(input_directory: str, start_date, end_date, hashtags: list) -> list:
+def process_tweets(input_directory: str, start_date, end_date, hashtags: list) -> list:
     tweets = []
-    process_directory(input_directory, start_date, end_date, hashtags, tweets)
+    if input_directory.endswith('.bz2'):
+        process_bz2_file(input_directory, start_date, end_date, hashtags, tweets)
+    else:
+        process_directory(input_directory, start_date, end_date, hashtags, tweets)
     return tweets
 
 def generate_graph_rt(tweets: list):
@@ -95,7 +66,7 @@ def generate_graph_rt(tweets: list):
                 G.add_edge(retweeted_user, retweeting_user)
         except (KeyError, TypeError) as e:
             print(f"Error processing tweet: {e}")
-    nx.write_gexf(G, 'retweet_graph.gexf')
+    nx.write_gexf(G, 'rt.gexf')
 
 def create_retweet_json(tweets: list):
     retweets = {}
@@ -122,7 +93,7 @@ def create_retweet_json(tweets: list):
             
     sorted_retweets = sorted(retweets.items(), key=lambda x: x[1]['receivedRetweets'], reverse=True)
     result = {"retweets": [{'username': key, **value} for key, value in sorted_retweets]}
-    with open('rt8.json', 'w') as f:
+    with open('rt.json', 'w') as f:
         json.dump(result, f, indent=4)
 
 def generate_graph_mention(tweets: list):
@@ -133,22 +104,45 @@ def generate_graph_mention(tweets: list):
             for mention in tweet['entities']['user_mentions']:
                 mentioned_user = mention['screen_name']
                 G.add_edge(tweeting_user, mentioned_user)
+    nx.write_gexf(G, 'mención.gexf')
     return G
+
+def generate_json_mention(tweets: list):
+    mentions_dict = defaultdict(lambda: OrderedDict([('username', ''), ('receivedMentions', 0), ('mentions', [])]))
+
+    for tweet in tweets:
+        if 'entities' in tweet and 'user_mentions' in tweet['entities'] and not tweet.get('retweeted_status'):
+            mentioning_user = tweet['user']['screen_name']
+            tweet_id = tweet.get('id')
+
+            for user_mention in tweet['entities']['user_mentions']:
+                mentioned_user = user_mention['screen_name']
+                mention_data = {'mentionBy': mentioning_user, 'tweets': [tweet_id]}
+
+                mentions_dict[mentioned_user]['receivedMentions'] += 1
+                mentions_dict[mentioned_user]['mentions'].append(mention_data)
+                mentions_dict[mentioned_user]['username'] = mentioned_user
+
+    # Organizar la lista de menciones por 'receivedMentions' de mayor a menor
+    sorted_mentions = sorted(mentions_dict.values(), key=lambda x: x['receivedMentions'], reverse=True)
+
+    with open('mención.json', 'w') as f:
+        json.dump(sorted_mentions, f, indent=4)
 
 
 def main(argv):
+    ti = time.time()
     input_directory = '/data'
     start_date = False
     end_date = False
     hashtags = []
     
-    args = argv.split()
     opts = []
     
     i = 0
-    while i < len(args):
-        argumento = args[i]
-        valor = args[i + 1] if i + 1 < len(args) else ''
+    while i < len(argv):
+        argumento = argv[i]
+        valor = argv[i + 1] if i + 1 < len(argv) else ''
         if argumento.startswith('--'):
             opts.append((argumento, ''))
         else:
@@ -171,7 +165,7 @@ def main(argv):
             with open(arg, 'r') as file:
                 hashtags = [line.strip() for line in file]
     
-    tweets = process_tweets2(input_directory, start_date, end_date, hashtags)
+    tweets = process_tweets(input_directory, start_date, end_date, hashtags)
     print('tweet procesados: ' + str(len(tweets)))
     
     for opt, arg in opts:
@@ -182,13 +176,15 @@ def main(argv):
         if opt == '--gm':
             generate_graph_mention(tweets)
         if opt == '--jm':
-            pass
-            #generate_json_mention()
+            generate_json_mention(tweets)
         if opt == '--gcrt':
             pass
             #generate_graph_corretweet()
         if opt == '--jcrt':
             pass
             #generate_json_corretweet()
+    tf = time.time()
+    print(tf - ti)
 
-main("-d  data/2016/01/06/00 -fi 06-01-16 --jrt --grt")
+if __name__ == "__main__":
+    main(sys.argv[1:])
